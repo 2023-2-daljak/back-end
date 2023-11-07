4b77f716a8ebde3a5bff5d0231c4d3b75ef51e36
dongyu-youn <donghe1472@naver.com>
2023-11-07 11:26:22 +0900

    [Feat] product 화면 디자인

--- products/views.py
+++ products/views.py
@@ -3,11 +3,12 @@ from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
-
+from django.core.paginator import Paginator
 from .serializers import ProductSerializer
 from django.views.generic import ListView
 from . import models
 from datetime import datetime
+from django.shortcuts import render, redirect
 
 
 class HomeView(ListView):
@@ -18,3 +19,12 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     context_object_name = "rooms"
+
+
+def all_products(request):
+    page = request.GET.get("page")
+    all_product = models.Product.objects.all()
+    paginator = Paginator(all_product, 2)
+    products = paginator.get_page(page)
+    print(products)
+    return render(request, "products/products.html", context={"potato": products})
