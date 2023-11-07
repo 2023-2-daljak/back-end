26e84decc4827ad3db1813b7cc04fb0d39a26c4a
dongyu-youn <>
2023-11-07 15:54:39 +0900

    [Feat]홈화면 완성중

--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
