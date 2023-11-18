ac5fe514cf712073fdf727e56f064f39a0ede3a4
dongyu-youn <>
2023-11-19 00:49:32 +0900

    [Feat] detail 페이지 수정

--- static/js/pic-fav.js
+++ static/js/pic-fav.js
@@ -22,3 +22,50 @@ bottomHearts.forEach((bottomHeart) => {
     relatedTopHeart.style.display = "block";
   });
 });
+
+// 편집 아이콘에 대한 클릭 이벤트 처리
+document.getElementById("editIcon").addEventListener("click", function () {
+  // 특정 URL로 이동
+  window.location.href = "{% url 'product:edit' product.pk %}";
+});
+
+// 삭제 아이콘에 대한 클릭 이벤트 처리
+document.getElementById("deleteIcon").addEventListener("click", function () {
+  // 특정 URL로 이동
+  window.location.href = "{% url 'product:delete' product.pk %}";
+});
+
+// 삭제 아이콘에 대한 클릭 이벤트 처리
+document.getElementById("messageIcon").addEventListener("click", function () {
+  // 특정 URL로 이동
+  console.log("hello");
+  window.location.href = "{% url 'conversations:detail' product.pk %}";
+});
+
+document.getElementById("favIcon").addEventListener("click", function () {
+  // 특정 URL로 이동
+  console.log("hello");
+  window.location.href = "{% url 'lists:toggle_room' product.id %}";
+});
+
+document.getElementById("favsIcon").addEventListener("click", function () {
+  // 특정 URL로 이동
+  console.log("hello");
+  window.location.href = "{% url 'lists:toggle_room' product.id %}";
+});
+
+// DOM이 준비되면 실행될 함수
+document.addEventListener("DOMContentLoaded", function () {
+  // 첫 번째 아이콘을 선택합니다.
+  const favsIcon1 = document.getElementById("favsIcon1");
+
+  // 클릭 이벤트 리스너 등록
+  favsIcon1.addEventListener("click", function () {
+    // active 클래스를 토글
+    if (favsIcon1.classList.contains("active")) {
+      favsIcon1.classList.remove("active");
+    } else {
+      favsIcon1.classList.add("active");
+    }
+  });
+});
