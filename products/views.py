ad1e02e3758f1dfcb52224660681fdd12124e81d
dongyu-youn <donghe1472@naver.com>
2023-11-27 18:07:10 +0900

    [Feat] merge

--- products/views.py
+++ products/views.py
@@ -47,37 +47,107 @@ def search(request):
     color_s = models.Grade.objects.all()
     other_s = models.Repair.objects.all()
 
-    department = request.GET.getlist("department")
-    species = request.GET.getlist("species")
-    grade = request.GET.getlist("grade")
-    repair = request.GET.getlist("repair")
+    shapes = request.GET.getlist("shape")
+    colors = request.GET.getlist("color")
+    print(colors)
+    minds = request.GET.getlist("mind")
+    others = request.GET.getlist("other")
 
     filter_args = {}
 
-    if len(department) > 0:
+    if len(shapes) > 0:
+        for s_amenity in shapes:
+            filter_args["department"] = int(s_amenity)
 
-        for s_amenity in department:
-            filter_args["department__pk"] = int(s_amenity)
+    if len(colors) > 0:
+        for s in colors:
+            filter_args["species"] = int(s)
 
-    if len(species) > 0:
-        for s in species:
-            filter_args["species__pk"] = int(s)
+    if len(minds) > 0:
+        for abc in minds:
+            filter_args["grade"] = int(abc)
 
-    if len(grade) > 0:
-        for abc in grade:
-            filter_args["grade__pk"] = int(abc)
-
-    if len(repair) > 0:
-        for oo in repair:
-            filter_args["repair__pk"] = int(oo)
+    if len(others) > 0:
+        for oo in others:
+            filter_args["repair"] = int(oo)
 
     if 'city' in request.GET:
         filter_args["title__contains"] = city
     picture = models.Product.objects.all().filter(**filter_args)
 
+    # search_history = None
+    # if city:
+    #     search_his
+    #
+    #
+    #
+    # tory = models.SearchHistory(query=city, user=request.user)
+    #     search_history.save()
+
+    # if city and request.user.is_authenticated:
+    #     search_history_list = models.SearchHistory.objects.filter(
+    #         query=city, user=request.user)
+    #     if search_history_list.exists():  # 조건에 맞는 객체가 존재하는지 확인
+    #         search_history = search_history_list.first()  # 첫 번째 객체 선택
+    #         search_history.count += 1
+    #         search_history.save()
+    #     else:
+    #         # 새로운 검색어라면 객체를 생성하여 저장
+    #         search_history = models.SearchHistory(
+    #             query=city, user=request.user, count=1)
+    #         search_history.save()
+
+    # search_history_list = models.SearchHistory.objects.order_by(
+    #     '-timestamp')[:8]
+
     query = request.GET.get("query")
 
     return render(request, "partials/search.html", {"abc": picture, "mind": mind_s, "color": color_s, "other": other_s, "shape": shapes_s, "city": city, 'query': query})
+
+
+class ProductDetail(DetailView):
+    model = models.Product
+    context_object_name = "product"
+
+
+class CreateProductView(user_mixins.LoggedInOnlyView, FormView):
+
+    form_class = forms.CreateProductForm
+    template_name = "products/product_create.html"
+
+    def form_valid(self, form):
+        project = form.save()
+        project.registrant = self.request.user
+        project.save()
+        # project.success(self.request, "Photo Uploaded")
+        return redirect(reverse("product:product_list"))
+
+
+class EditProjectView(UpdateView):
+
+    model = models.Product
+    template_name = "products/product_edit.html"
+    fields = (
+        "title",
+        "content",
+        "profile",
+        "department",
+        "species",
+        "grade",
+        "repair",
+    )
+
+    def get_success_url(self):
+        return reverse("product:product_list")
+
+
+class DeleteProductView(DeleteView):
+
+    model = models.Product
+    template_name = "products/product_delete.html"
+
+    def get_success_url(self):
+        return reverse("product:product_list")
 
 
 class ProductDetail(DetailView):
